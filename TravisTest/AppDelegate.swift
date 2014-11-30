f47f872c1d34472814a49c6a8060f6a8eb5d5400
Chris Eidhof <chris@eidhof.nl>
2014-11-30 16:50:51 -0500

    Initial commit

---
+++ TravisTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TravisTest
+//
+//  Created by Chris Eidhof on 30/11/14.
+//  Copyright (c) 2014 Chris Eidhof. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TravisTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TravisTest
+//
+//  Created by Chris Eidhof on 30/11/14.
+//  Copyright (c) 2014 Chris Eidhof. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
